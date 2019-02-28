d48aa48cdeebfcd088b750189bbea78e2b516ea3
kapsi <kapsiakk@gmail.com>
2019-02-28 11:34:25 +0100

    blab

--- client.js
+++ client.js
@@ -1,1 +1,5 @@
-fetch('localhost:7777').then(res => res.text()).then(res => console.log(res));
+fetch('http://localhost:7777', {
+    method: "GET",
+    // mode: "no-cors",
+    cache: "no-cache"
+}).then(res => res.text()).then(res => console.log('res', res));
--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 var http = require('http');
 http.createServer(function (req, res) {
   console.log('request', req.path);
-  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.setHeader('Content-Type', 'text/plain');
+  // res.writeHead(200, {'Content-Type': 'text/plain'});
   res.write('Hello World!');
   res.end();
 }).listen(7777);
